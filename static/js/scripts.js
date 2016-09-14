afa172225dc79b66d3590348382237770628394d
Josiah Rooney <josiahrooney@gmail.com>
2016-09-14 14:17:15 -0700

    added new code examlpes

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,31 +1,49 @@
 $("#codeTabs a").click(function (e) {
-	e.preventDefault();
-	$(this).tab("show");
+    e.preventDefault();
+    $(this).tab("show");
 });
 
 
 $('.code-block').each(function(index){
-	if (index < 10) {
-		$(this).find('h3').text("0"+index + ". " + $(this).find('h3').text());
-	} else {
-		$(this).find('h3').text(index + ". " + $(this).find('h3').text());
-	}
+    if (index < 10) {
+        $(this).find('h3').text("0"+index + ". " + $(this).find('h3').text());
+    } else {
+        $(this).find('h3').text(index + ". " + $(this).find('h3').text());
+    }
 });
+
 
 $('.mobile-button').click(function(){
-	if ( $('.navbar-nav').hasClass('closed') ) {
-		$('.navbar-nav').removeClass('closed');
-	} else {
-		$('.navbar-nav').addClass('closed');
-	}
+    if ( $('.navbar-nav').hasClass('closed') ) {
+        $('.navbar-nav').removeClass('closed');
+    } else {
+        $('.navbar-nav').addClass('closed');
+    }
 });
 
+
 $('.hover-info').hover(function(){
-	var info = $(this).data('hover');
-	$(this).append('<div class="hover-info-after">' + info + '</div>');
+    var info = $(this).data('hover');
+    $(this).append('<div class="hover-info-after">' + info + '</div>');
 }, function() {
-	$('.hover-info-after').remove();
+    $('.hover-info-after').remove();
 });
+
+
+$(document).ready(function(){
+    $('.language-python').each(function(){
+        $(this).prev().addClass('python');
+    });
+    $('.language-js').each(function(){
+        $(this).prev().addClass('js');
+    });
+});
+
+
+
+
+
+
 
 // Code Examples
 function reverseString(str) {
